8e3d61fa01d4452378efb8acb0e600b8c20b78d4
Utkarsh Singh <109547510+utkarshrajput7@users.noreply.github.com>
2023-08-11 04:33:04 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+const jokeContainer = document.querySelector(".joketext");
+const btn = document.querySelector(".btn");
+
+const url = "https://v2.jokeapi.dev/joke/Programming,Pun?blacklistFlags=nsfw,religious,political,racist,sexist,explicit&type=single";
+
+let getJoke = ()=>{
+    jokeContainer.classList.remove("fade");
+    fetch(url)
+    .then(data => data.json())
+    .then(item => {
+        jokeContainer.textContent = `${item.joke}`;
+        jokeContainer.classList.add("fade");
+    });
+}
+
+btn.addEventListener("click",getJoke);
+
+getJoke();
